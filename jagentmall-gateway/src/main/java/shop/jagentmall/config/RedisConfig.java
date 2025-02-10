951a8e3c47c95d08f719e53ce1662e9a2cb79108
lee <3216728546@qq.com>
2025-02-10 14:14:40 +0800

    完成了gateway模块的搭建

---
+++ jagentmall-gateway/src/main/java/shop/jagentmall/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package shop.jagentmall.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redis相关配置
+ */
+@Configuration
+public class RedisConfig extends BaseRedisConfig {
+
+}
